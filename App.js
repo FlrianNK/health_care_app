3498b928a8dbac6fdc089d8a34a8902403bad466
TQNLP <thiquynh-nhu.pham@edu.esiee.fr>
2023-06-06 17:50:19 +0200

    added 1st page (functional)

--- App.js
+++ App.js
@@ -1,19 +1,197 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { NavigationContainer } from 'react-navigation/native';
-import { createBottomTabNavigator } from 'react-navigation/bottom-tabs';
+import React, { useState, useEffect } from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  Button,
+  TouchableWithoutFeedback,
+  Keyboard,
+  ScrollView,
+} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Picker } from '@react-native-picker/picker';
+import { Image } from 'react-native';
 
+// This is the main function for the Health Goals Screen
 function HealthGoalsScreen() {
+  // Setting up global states for age, gender, height, weight, activity level and health goal
+  const [lAge, setAge] = useState('');
+  const [lGender, setGender] = useState(null);
+  const [lHeight, setHeight] = useState('');
+  const [lWeight, setWeight] = useState('');
+  const [lActivityLevel, setActivityLevel] = useState(null);
+  const [lHealthGoal, setHealthGoal] = useState(null);
+
+  // Checking if all the form fields are filled correctly
+  const lIsFormValid =
+    lAge !== '' &&
+    lGender !== null &&
+    lGender !== 'Select your gender' &&
+    lHeight !== '' &&
+    lWeight !== '' &&
+    lActivityLevel !== null &&
+    lActivityLevel !== 'Select your activity level' &&
+    lHealthGoal !== null &&
+    lHealthGoal !== 'Select your health goal';
+
+  // Setting up state for caloric intake and show results
+  const [lCaloricIntake, setCaloricIntake] = useState(0);
+  const [lShowResults, setShowResults] = useState(false);
+
+  // Function to handle input change
+  const handleInputChange = (setter) => (value) => {
+    setShowResults(false);
+    setter(value);
+  };
+
+  // Use effect function to calculate caloric intake when all form fields are filled
+  useEffect(() => {
+    if (lIsFormValid) {
+      calculateCaloricIntake();
+    }
+  }, [lAge, lGender, lHeight, lWeight, lActivityLevel, lHealthGoal]);
+
+  // Function to calculate BMR
+  const calculateBMR = () => {
+    let lBMR;
+    if (lGender === 'male') {
+      lBMR = 88.362 + 13.397 * lWeight + 4.799 * lHeight - 5.677 * lAge;
+    } else {
+      lBMR = 447.593 + 9.247 * lWeight + 3.098 * lHeight - 4.33 * lAge;
+    }
+    return lBMR;
+  };
+
+  // Function to calculate caloric intake
+  const calculateCaloricIntake = () => {
+    let lBMR = calculateBMR();
+    let lActivityFactor = 1.2;
+    switch (lActivityLevel) {
+      case 'light':
+        lActivityFactor = 1.375;
+        break;
+      case 'moderate':
+        lActivityFactor = 1.55;
+        break;
+      case 'heavy':
+        lActivityFactor = 1.725;
+        break;
+      case 'extra':
+        lActivityFactor = 1.9;
+        break;
+    }
+    let lTotalCalories = lBMR * lActivityFactor;
+    switch (lHealthGoal) {
+      case 'loss':
+        lTotalCalories -= 500;
+        break;
+      case 'gain':
+        lTotalCalories += 500;
+        break;
+    }
+    setCaloricIntake(Math.round(lTotalCalories));
+  };
+
   return (
-    <View style={styles.text}>
-      <Text>Health Goals!</Text>
-    </View>
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <View style={styles.container}>
+        <ScrollView style={styles.scrollview} keyboardShouldPersistTaps="handled">
+          <Text style={styles.label}>Age:</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={handleInputChange(setAge)}
+            value={lAge}
+            keyboardType="numeric"
+            maxLength={3}
+            minLength={1}
+          />
+
+          <Text style={styles.label}>Gender:</Text>
+          <Picker
+            style={styles.picker}
+            selectedValue={lGender}
+            prompt="Select your gender"
+            onValueChange={handleInputChange(
+              (itemValue) => itemValue !== 'default' && setGender(itemValue)
+            )}>
+            <Picker.Item label="Select your gender" value="default" />
+            <Picker.Item label="Male" value="male" />
+            <Picker.Item label="Female" value="female" />
+          </Picker>
+
+          <Text style={styles.label}>Height (cm):</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={handleInputChange(setHeight)}
+            value={lHeight}
+            keyboardType="numeric"
+            maxLength={3}
+            minLength={1}
+          />
+
+          <Text style={styles.label}>Weight (kg):</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={handleInputChange(setWeight)}
+            value={lWeight}
+            keyboardType="numeric"
+            maxLength={3}
+            minLength={1}
+          />
+
+          <Text style={styles.label}>Activity Level:</Text>
+          <Picker
+            style={styles.picker}
+            selectedValue={lActivityLevel}
+            prompt="Select your activity level"
+            onValueChange={handleInputChange(
+              (itemValue) => itemValue !== 'default' && setActivityLevel(itemValue)
+            )}>
+            <Picker.Item label="Select your activity level" value="default" />
+            <Picker.Item label="Sedentary" value="sedentary" />
+            <Picker.Item label="Light Exercise" value="light" />
+            <Picker.Item label="Moderate Exercise" value="moderate" />
+            <Picker.Item label="Heavy Exercise" value="heavy" />
+            <Picker.Item label="Extra Active" value="extra" />
+          </Picker>
+
+          <Text style={styles.label}>Health Goal:</Text>
+          <Picker
+            style={styles.picker}
+            selectedValue={lHealthGoal}
+            prompt="Select your Health goal"
+            onValueChange={handleInputChange(
+              (itemValue) => itemValue !== 'default' && setHealthGoal(itemValue)
+            )}>
+            <Picker.Item label="Select your health goal" value="default" />
+            <Picker.Item label="Weight Loss" value="loss" />
+            <Picker.Item label="Weight Maintenance" value="maintenance" />
+            <Picker.Item label="Weight Gain" value="gain" />
+          </Picker>
+
+          <Button
+            title="Submit"
+            onPress={() => {
+              setShowResults(true);
+            }}
+            disabled={!lIsFormValid}
+          />
+        </ScrollView>
+        {lShowResults && (
+          <View style={styles.resultContainer}>
+            <Text style={styles.resultText}>Daily Caloric Intake: {lCaloricIntake}</Text>
+          </View>
+        )}
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
 function FoodDatabaseScreen() {
   return (
-    <View style={styles.text}>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Food Database!</Text>
     </View>
   );
@@ -21,29 +199,90 @@ function FoodDatabaseScreen() {
 
 function MealPlanningScreen() {
   return (
-    <View style={styles.text}>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Meal Planning!</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  text:{
-    flex: 1, 
-    justifyContent: 'center', 
-    alignItems: 'center',
-  }
-})
-
+  scrollview: {
+    paddingRight: 10,
+  },
+  container: {
+    flex: 1,
+    padding: 20,
+    justifyContent: 'center',
+  },
+  label: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginTop: 10,
+    marginBottom: 10,
+    color: '#293FA6',
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ddd',
+    padding: 10,
+    fontSize: 18,
+    borderRadius: 6,
+  },
+  picker: {
+    borderWidth: 1,
+    borderColor: '#ddd',
+    borderRadius: 6,
+    marginBottom: 15,
+    overflow: 'hidden',
+  },
+  logo: {
+    color: '#000000',
+    width: 10,
+    height: 10,
+  },
+});
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="Health Goals" component={HealthGoalsScreen} />
-        <Tab.Screen name="Food Database" component={FoodDatabaseScreen} />
-        <Tab.Screen name="Meal Planning" component={MealPlanningScreen} />
+        <Tab.Screen
+          name="Health Goals"
+          component={HealthGoalsScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Image
+                source={require('./goal.png')}
+                style={{ height: size, width: size, tintColor: color }}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Food Database"
+          component={FoodDatabaseScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Image
+                source={require('./diet.png')}
+                style={{ height: size, width: size, tintColor: color }}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Meal Planning"
+          component={MealPlanningScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Image
+                source={require('./calendar.png')}
+                style={{ height: size, width: size, tintColor: color }}
+              />
+            ),
+          }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
