e21f62aca5f4fa9ce70db8642b43b22cbd63d0e3
FlrianNK <florian.nor@edu.esiee.fr>
2023-06-14 11:16:00 +0200

    Revert "Merge branch 'main' of https://github.com/FlrianNK/health_care_app"
    This reverts commit f96ea235ddb879bb68bc1cc249f671e241300cc0, reversing
    changes made to e7d690767ab750b27a7908871e62fe3604cda72c.

--- App.js
+++ App.js
@@ -20,17 +20,267 @@ import { Image } from 'react-native';
 
 // This is the main function for the Health Goals Screen
 function HealthGoalsScreen() {
+  // Setting up global states for age, gender, height, weight, activity level and health goal
+  const [lAge, setAge] = useState('');
+  const [lGender, setGender] = useState(null);
+  const [lHeight, setHeight] = useState('');
+  const [lWeight, setWeight] = useState('');
+  const [lActivityLevel, setActivityLevel] = useState(null);
+  const [lHealthGoal, setHealthGoal] = useState(null);
+
+  // Checking if all the form fields are filled correctly
+  const lIsFormValid =
+    lAge !== '' &&
+    lGender !== null &&
+    lGender !== 'Select your gender' &&
+    lHeight !== '' &&
+    lWeight !== '' &&
+    lActivityLevel !== null &&
+    lActivityLevel !== 'Select your activity level' &&
+    lHealthGoal !== null &&
+    lHealthGoal !== 'Select your health goal';
+
+  // Setting up state for caloric intake and show results
+  const [lCaloricIntake, setCaloricIntake] = useState(0);
+  const [lShowResults, setShowResults] = useState(false);
+  const [lInputTextWeight, setInputText] = useState('');
+
+  // Function to handle input change
+  const handleInputChange = (setter) => (value) => {
+    setShowResults(false);
+    setter(value);
+  };
+
+  // Use effect function to calculate caloric intake when all form fields are filled
+  useEffect(() => {
+    if (lIsFormValid) {
+      calculateCaloricIntake();
+    }
+  }, [lAge, lGender, lHeight, lWeight, lActivityLevel, lHealthGoal]);
+
+  // Function to calculate BMR
+  const calculateBMR = () => {
+    let lBMR;
+    if (lGender === 'male') {
+      lBMR = 88.362 + 13.397 * lWeight + 4.799 * lHeight - 5.677 * lAge;
+    } else {
+      lBMR = 447.593 + 9.247 * lWeight + 3.098 * lHeight - 4.33 * lAge;
+    }
+    return lBMR;
+  };
+
+  // Function to calculate caloric intake
+  const calculateCaloricIntake = () => {
+    let lBMR = calculateBMR();
+    let lActivityFactor = 1.2;
+    switch (lActivityLevel) {
+      case 'light':
+        lActivityFactor = 1.375;
+        break;
+      case 'moderate':
+        lActivityFactor = 1.55;
+        break;
+      case 'heavy':
+        lActivityFactor = 1.725;
+        break;
+      case 'extra':
+        lActivityFactor = 1.9;
+        break;
+    }
+    let lTotalCalories = lBMR * lActivityFactor;
+    switch (lHealthGoal) {
+      case 'loss':
+        lTotalCalories -= 500;
+        break;
+      case 'gain':
+        lTotalCalories += 500;
+        break;
+    }
+    setCaloricIntake(Math.round(lTotalCalories));
+  };
+
   return (
-    <View style={styles.text}>
-      <Text>Health Goals!</Text>
-    </View>
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <View style={styles.container}>
+        <ScrollView style={styles.scrollview} keyboardShouldPersistTaps="handled">
+          <Text style={styles.label}>Age:</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={(text) => {
+              // Exclude unwanted characters
+              let lNewText = text.replace(/[^0-9]/g, '');
+              handleInputChange(lNewText);
+            }}
+            value={lAge}
+            keyboardType="numeric"
+            maxLength={3}
+            minLength={1}
+          />
+
+          <Text style={styles.label}>Gender:</Text>
+          <Picker
+            style={styles.picker}
+            selectedValue={lGender}
+            prompt="Select your gender"
+            onValueChange={handleInputChange(
+              (itemValue) => itemValue !== 'default' && setGender(itemValue)
+            )}>
+            <Picker.Item label="Select your gender" value="default" />
+            <Picker.Item label="Male" value="male" />
+            <Picker.Item label="Female" value="female" />
+          </Picker>
+          <Text style={styles.label}>Height (cm):</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={(text) => {
+              // Exclude unwanted characters
+              let lNewText = text.replace(/[^0-9]/g, '');
+              handleInputChange(lNewText);
+            }}
+            value={lHeight}
+            keyboardType="numeric"
+            maxLength={3}
+            minLength={1}
+          />
+          <Text style={styles.label}>Weight (kg):</Text>
+          <TextInput
+            style={styles.input}
+            value={lInputTextWeight}
+            onChangeText={(text) => {
+              // Allow numbers and a single decimal point
+              let newText = text.replace(/[^0-9\.]/g, '');
+              if (newText.split('.').length > 2) {
+                // Prevent more than one dot
+                return;
+              }
+              // Update the text manually
+              setInputText(newText);
+            }}
+            keyboardType="decimal-pad" // Use decimal-pad to allow both numbers and decimal point
+            maxLength={6} // Allow up to 3 digits before and after decimal point
+          />
+          <Text style={styles.label}>Activity Level:</Text>
+          <Picker
+            style={styles.picker}
+            selectedValue={lActivityLevel}
+            prompt="Select your activity level"
+            onValueChange={handleInputChange(
+              (itemValue) => itemValue !== 'default' && setActivityLevel(itemValue)
+            )}>
+            <Picker.Item label="Select your activity level" value="default" />
+            <Picker.Item label="Sedentary" value="sedentary" />
+            <Picker.Item label="Light Exercise" value="light" />
+            <Picker.Item label="Moderate Exercise" value="moderate" />
+            <Picker.Item label="Heavy Exercise" value="heavy" />
+            <Picker.Item label="Extra Active" value="extra" />
+          </Picker>
+          <Text style={styles.label}>Health Goal:</Text>
+          <Picker
+            style={styles.picker}
+            selectedValue={lHealthGoal}
+            prompt="Select your Health goal"
+            onValueChange={handleInputChange(
+              (itemValue) => itemValue !== 'default' && setHealthGoal(itemValue)
+            )}>
+            <Picker.Item label="Select your health goal" value="default" />
+            <Picker.Item label="Weight Loss" value="loss" />
+            <Picker.Item label="Weight Maintenance" value="maintenance" />
+            <Picker.Item label="Weight Gain" value="gain" />
+          </Picker>
+          <Button
+            title="Submit"
+            onPress={() => {
+              setShowResults(true);
+            }}
+            disabled={!lIsFormValid}
+          />
+        </ScrollView>
+        {lShowResults && (
+          <View style={styles.resultContainer}>
+            <Text style={styles.resultText}>Daily Caloric Intake: {lCaloricIntake}</Text>
+          </View>
+        )}
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
 function FoodDatabaseScreen() {
-  return (
-    <View style={styles.text}>
-      <Text>Food Database!</Text>
+  const [lSearchQuery, setLSearchQuery] = useState('');
+  const [lFoods, setLFoods] = useState([]);
+  const [lMealPlan, setLMealPlan] = useState({
+    Breakfast: [],
+    Lunch: [],
+    Snack: [],
+    Dinner: [],
+  });
+  const [lSelectedMeal, setLSelectedMeal] = useState('');
+  const [lModalVisible, setLModalVisible] = useState(false);
+  const handleInputChange = (value) => {
+    setLSearchQuery(value);
+  };
+  const [lSelectedFoodItem, setLSelectedFoodItem] = useState(null);
+
+  const handleSearch = async () => {
+    try {
+      const response = await fetch(
+        `https://api.edamam.com/api/food-database/v2/parser?ingr=${lSearchQuery}&app_id=97b53550&app_key=7e926f3e50058f664119bca13c924cba`
+      );
+
+      if (!response.ok) {
+        throw new Error('HTTP error ' + response.status);
+      }
+      const data = await response.json();
+      if (data && data.hints && data.hints.length === 0) {
+        throw new Error('No foods found');
+      }
+
+      // Create a map for storing unique food items
+      let foodMap = new Map();
+
+      data.hints.forEach((item) => {
+        let { foodId, label, category, nutrients } = item.food;
+
+        // Round each nutritional value to next whole number
+        // to compare them afterwards to avoid duplicates items
+        let roundedNutrients = {
+          ENERC_KCAL: Math.ceil(nutrients.ENERC_KCAL),
+          CHOCDF: Math.ceil(nutrients.CHOCDF),
+          FAT: Math.ceil(nutrients.FAT),
+          FIBTG: Math.ceil(nutrients.FIBTG),
+          PROCNT: Math.ceil(nutrients.PROCNT),
+        };
+
+        // Create a unique identifier
+        let identifier = `${foodId}-${label}-${category}-${roundedNutrients.ENERC_KCAL}-${roundedNutrients.CHOCDF}-${roundedNutrients.FAT}-${roundedNutrients.FIBTG}-${roundedNutrients.PROCNT}`;
+
+        // If this is a new identifier, store the item and roundedNutrients in the map
+        if (!foodMap.has(identifier)) {
+          foodMap.set(identifier, { ...item, roundedNutrients });
+        }
+      });
+
+      // Convert the map values to an array
+      let uniqueFoods = Array.from(foodMap.values());
+
+      setLFoods(uniqueFoods);
+    } catch (error) {
+      console.error(error);
+      Alert.alert('Error', error.message);
+    }
+  };
+
+  const renderItem = ({ item }) => (
+    <View>
+      <Image source={{ uri: item.food.image }} style={styles.image} />
+      <Text style={styles.foodTitle}>{item.food.label}</Text>
+      <Text>Category: {item.food.category}</Text>
+      <Text>Calories: {item.roundedNutrients.ENERC_KCAL} kcal</Text>
+      <Text>Carbs: {item.roundedNutrients.CHOCDF} g</Text>
+      <Text>Fat: {item.roundedNutrients.FAT} g</Text>
+      <Text>Fiber: {item.roundedNutrients.FIBTG} g</Text>
+      <Text>Protein: {item.roundedNutrients.PROCNT} g</Text>
+      <Button style={styles.button} title="Add to Meal Plan" onPress={() => addToMealPlan(item)} />
     </View>
   );
 
@@ -61,8 +311,35 @@ function FoodDatabaseScreen() {
   };
 
   return (
-    <View style={styles.text}>
-      <Text>Meal Planning!</Text>
+    <View style={styles.container}>
+      <TextInput
+        value={lSearchQuery}
+        onChangeText={handleInputChange}
+        placeholder="Search for a food..."
+      />
+      <TouchableOpacity style={styles.button} onPress={handleSearch}>
+        <Text style={styles.buttonText}>Search</Text>
+      </TouchableOpacity>
+      <FlatList
+        data={lFoods}
+        keyExtractor={(item, index) => item.food.foodId + '-' + index}
+        renderItem={renderItem}
+      />
+      <Modal animationType="slide" transparent={true} visible={lModalVisible}>
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <Text style={styles.modalText}>For which meal do you want to add the item to ?</Text>
+            <Picker selectedValue={lSelectedMeal} onValueChange={handleMealSelection}>
+              <Picker.Item label="Select meal type" value="" color="black" />
+              <Picker.Item label="Breakfast" value="Breakfast" color="black" />
+              <Picker.Item label="Lunch" value="Lunch" color="black" />
+              <Picker.Item label="Dinner" value="Dinner" color="black" />
+              <Picker.Item label="Snack" value="Snack" color="black" />
+            </Picker>
+            <Button title="Cancel" onPress={handleModalClose} />
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 }
